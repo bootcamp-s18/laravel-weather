4860e9e7b55c71668e970d71c84675691b14b09d
Janine Hempy <jhempy@gmail.com>
2018-07-17 13:50:03 -0400

    WIP - still troubleshooting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Weather</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        @if (Request::secure())
-            <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        @else
-            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        @endif
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->
         <style>
