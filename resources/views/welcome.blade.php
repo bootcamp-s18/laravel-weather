b0a4f83bd6ce5a01b4be05f39a903ff630278487
Janine Hempy <jhempy@gmail.com>
2018-07-17 11:50:27 -0400

    Adding back assets for heroku troubleshooting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <title>Weather</title>
 
         <!-- Styles -->
-        <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->
         <style>
