3ab3b9f8ffdd053496a70db5ac5dc20f4bc7690d
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2018-07-26 09:21:31 -0400

    Refactored our tests to use chai instead of NodeJS built-in assert

--- tests/JS/utils.test.js
+++ tests/JS/utils.test.js
@@ -1,36 +1,39 @@
-var assert = require('assert');
+var chai = require('chai');
+
 var utils = require('../../resources/assets/js/utils.js');
 var convertKtoF = utils.convertKtoF;
 var convertKtoC = utils.convertKtoC;
+
+chai.should();
 
 describe("convertKtoF function", function() {
 
     describe("converts from kelvin to fahrenheit", function() {
 
         it ("the lowest temperature recorded on earth", function() {
-            assert.equal(convertKtoF(184), -128);
+            convertKtoF(184).should.equal(-128);
         });
 
         it ("the freezing point of water", function() {
-            assert.equal(convertKtoF(273.15), 32);
+            convertKtoF(273.15).should.equal(32);
         });
 
         it ("the average surface temperature on earth", function() {
-            assert.equal(convertKtoF(288), 59);
+            convertKtoF(288).should.equal(59);
         });
 
         it ("the highest temperature recorded on earth", function() {
-            assert.equal(convertKtoF(331), 136);
+            convertKtoF(331).should.equal(136);
         });
     });
 
     it ("throws TypeError when passed null or undefined", function() {
-        assert.throws(function() { convertKtoF(null); });
-        assert.throws(convertKtoF);
+        (function() { convertKtoF(null); }).should.throw(TypeError);
+        (function() { convertKtoF(); }).should.throw(TypeError);
     });
 
     it ("converts string arguments to numbers", function() {
-        assert.equal(convertKtoF("184"), -128);
+        convertKtoF("184").should.equal(-128);
     });
 });
 
@@ -39,28 +42,28 @@ describe("convertKtoC function", function() {
     describe("converts from kelvin to celsius", function() {
 
         it ("the lowest temperature recorded on earth", function() {
-            assert.equal(convertKtoC(184), -89);
+            convertKtoC(184).should.equal(-89);
         });
 
         it ("the freezing point of water", function() {
-            assert.equal(convertKtoC(273.15), 0);
+            convertKtoC(273.15).should.equal(0);
         });
 
         it ("the average surface temperature on earth", function() {
-            assert.equal(convertKtoC(288), 15);
+            convertKtoC(288).should.equal(15);
         });
 
         it ("the highest temperature recorded on earth", function() {
-            assert.equal(convertKtoC(331), 58);
+            convertKtoC(331).should.equal(58);
         });
     });
 
     it ("throws TypeError when passed null or undefined", function() {
-        assert.throws(function() { convertKtoC(null); });
-        assert.throws(convertKtoF);
+        (function() { convertKtoC(null); }).should.throw(TypeError);
+        (function() { convertKtoC(); }).should.throw(TypeError);
     });
 
     it ("converts string arguments to numbers", function() {
-        assert.equal(convertKtoC("184"), -89);
+        convertKtoC("184").should.equal(-89);
     });
 });
